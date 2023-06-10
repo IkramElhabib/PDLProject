b1836deec055220f1d7727278789fd2a18b45141
unknown <ikram.elhabib@sesame.com.tn>
2023-06-10 16:17:49 +0100

    role updated successfully

--- src/main/java/com/example/demo/Controller/RoleController.java
+++ src/main/java/com/example/demo/Controller/RoleController.java
@@ -1,5 +1,6 @@
 package com.example.demo.Controller;
 
+import java.util.List;
 import java.util.Random;
 import java.util.UUID;
 
@@ -10,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -51,7 +53,30 @@ public class RoleController {
         roleRepository.save(role);
         
         return "redirect:/roles";
-    }    
+    }
+    @GetMapping
+    public String showRoleList(Model model) {
+        List<Role> roles = roleRepository.findAll();
+        model.addAttribute("roles", roles);
+        
+        return "listroles";
+    }
+    @GetMapping("/edit/{id}")
+    public String showUpdateRoleForm(@PathVariable("id") Long id, Model model) {
+        Role role = roleRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid role id: " + id));
+        model.addAttribute("role", role);
+        return "updaterole";
+    }
+    @PostMapping("/update")
+    public String updateRole(@ModelAttribute("role") @Valid Role role, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "updaterole";
+        }
+        
+        roleRepository.save(role);
+        
+        return "redirect:/roles";
+    }
     private long generateRandomId() {
         Random random = new Random();
         return random.nextLong() & Long.MAX_VALUE;
